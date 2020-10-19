415d3c70e865b80e04549eea993f51a7e3bc1a24
Aditya Kote <53165193+adityaKoteCoder@users.noreply.github.com>
2020-10-20 03:57:32 +0530

    second

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    })
+});
